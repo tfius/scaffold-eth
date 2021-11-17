82a9020f69a7996fcf5c777810982ae10ef9899b
tfius <tadej.fius@gmail.com>
2021-11-17 23:38:58 +0100

    staking, exchange, buy&sell DMs

---
+++ packages/hardhat/contracts/Goldinar.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+ 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract Goldinar is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("Goldinar", "DNAR") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); 
+    } 
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not the minter");
+        _mint(to, amount);
+    } 
+}
