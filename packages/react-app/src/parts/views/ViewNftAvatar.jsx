2c1da93fc22de5fc25bd37c14ceb0b21fb4cfba3
tfius <tadej.fius@gmail.com>
2022-04-25 00:11:08 +0200

    fix viewNftAvatar view, so it displays account address nft

--- packages/react-app/src/parts/views/ViewNftAvatar.jsx
+++ packages/react-app/src/parts/views/ViewNftAvatar.jsx
@@ -1,23 +1,14 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 import { Button, Card, Input, Tooltip, Row } from "antd";
-import { notification } from "antd";
-import { ethers } from "ethers";
 import * as helpers from "../helpers";
-import { uploadJsonToBee } from "../SwarmUpload/BeeService";
-import FileUpload from "../SwarmUpload/FileUpload";
-import { useStore } from "../../state";
 
 export default function ViewNftAvatar(props) {
-  const {
-    state: { post, file },
-    dispatch,
-  } = useStore();
-
   const history = useHistory();
-  const [seconds, setSeconds] = useState(0);
   const [avatar, setAvatar] = useState(null);
   const [avatarInfo, setAvatarInfo] = useState(null);
+  const [eligible, setEligible] = useState([]); // eligibleTokens
+
   let { ownerAddress, tokenId } = useParams();
 
   const {
@@ -35,27 +26,41 @@ export default function ViewNftAvatar(props) {
     url,
   } = props;
 
+  async function getData() {
+    if (eligible.length != 0) return;
+    const res = await fetch("eligableTokens.json");
+    const data = await res.json();
+    console.log("got eligable tokens", data);
+    setEligible(data);
+  }
+
   async function getAvatar() {
     if (readContracts === undefined) return;
     if (readContracts.WAMSpring2022 === undefined) return;
 
     try {
-    const info = await readContracts.WAMSpring2022.getAvatarInfo(tokenId);
-    const url = await readContracts.WAMSpring2022.tokenURI(tokenId);
+      //debugger;
+      // token id is acctualy original id of resistance avatar not id of token
+      const id = eligible.find(x => x.owner === ownerAddress);
+      const idx = eligible.findIndex(o => o.owner === ownerAddress);
+      console.log("id", id, idx);  
 
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data, info, url);
-    setAvatar(data);
-    setAvatarInfo(info);
-    } catch(e)
-    {
+      const info = await readContracts.WAMSpring2022.getAvatarInfo(idx); //tokenId);
+      const url = await readContracts.WAMSpring2022.tokenURI(idx);
+
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data, info, url);
+
+      setAvatar(data);
+      setAvatarInfo(info);
+    } catch (e) {
       console.error(e);
     }
   }
 
   useEffect(() => {
-    // getAvatar();
+    getData();
   }, []);
   useEffect(() => {
     getAvatar();
@@ -65,15 +70,24 @@ export default function ViewNftAvatar(props) {
 
   return (
     <div style={{ maxWidth: 800, margin: "auto", marginTop: 5, paddingBottom: 25, lineHeight: 1.5 }}>
-      <Card title={<Card.Meta title={<h2  onClick={()=>helpers.speak("WeAreMillions Spring Hackathon 2022")}>{avatar.description}</h2>} />}>
-        <Card title={<h1 onClick={()=>helpers.speak(avatar.name)}>{avatar.name}</h1>} type="inner">
+      <Card
+        title={
+          <Card.Meta
+            title={<h2 onClick={() => helpers.speak("WeAreMillions Spring Hackathon 2022")}>{avatar.description}</h2>}
+          />
+        }
+      >
+        <Card title={<h1 onClick={() => helpers.speak(avatar.name)}>{avatar.name}</h1>} type="inner">
           <div style={{ background: "white", height: "30vmax", maxWidth: "100%", maxHeight: "250px" }}>
             <img src={avatar.image} style={{ maxWidth: "100%", maxHeight: "250px", objectFit: "scale-down" }} />
           </div>
 
           {avatar.attributes.map((attribute, i) => (
             <Card.Grid key={"parent_" + i} style={{ maxHeight: "90px", minHeight: "70px" }}>
-              <div style={{ lineHeight: "1.5rem" }} onClick={()=>helpers.speak(attribute.trait_type + "...." + attribute.value + " points")}>
+              <div
+                style={{ lineHeight: "1.5rem" }}
+                onClick={() => helpers.speak(attribute.trait_type + "...." + attribute.value + " points")}
+              >
                 {attribute.trait_type}
                 <br />
                 {attribute.value}
