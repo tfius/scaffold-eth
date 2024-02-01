f6bb05fc7e8db46433c2d01e202ac5675a4210ee
tfius <tadej.fius@gmail.com>
2024-02-01 22:25:31 +0100

    TestToken

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -1,61 +1,79 @@
 // deploy/00_deploy_your_contract.js
 
-const { ethers } = require('hardhat')
+const { ethers } = require("hardhat");
 
-const localChainId = '31337' 
+const localChainId = "31337";
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
-  const { deploy } = deployments
-  const { deployer } = await getNamedAccounts()
-  const chainId = await getChainId() 
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
 
   // deployer 0xd27ffA0e47Fca8D3E757B4d2C408169859B8c419
 
   console.log("ChainId", chainId);
   console.log("deployer", deployer);
 
-  console.log('SwarmMail')
-  const SwarmMail = await deploy('SwarmMail', {
+  console.log("TestToken");
+  const Token = await deploy("TestToken", {
+    from: deployer,
+    log: true,
+    args: ["localBZZ", "LBZZ", "1000000000000000000000000000"],
+  });
+
+  // transfer tokens to deployer
+  const token = await ethers.getContract("TestToken");
+  await token.transfer(
+    "0x1894F06a48acD00A2793A0eB00FFE7B6184B630e",
+    ethers.utils.parseEther("1000000")
+  );
+  await token.transfer(deployer, ethers.utils.parseEther("1000000"));
+
+  return;
+
+  console.log("SwarmMail");
+  const SwarmMail = await deploy("SwarmMail", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
-  console.log('DataHub')
-  const DataHub = await deploy('DataHub', {
+  });
+  console.log("DataHub");
+  const DataHub = await deploy("DataHub", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
-  console.log('Calendar')
-  const Calendar = await deploy('Calendar', {
+  });
+  console.log("Calendar");
+  const Calendar = await deploy("Calendar", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
-  console.log('Scheduler')
-  const Scheduler = await deploy('Scheduler', {
+  });
+  console.log("Scheduler");
+  const Scheduler = await deploy("Scheduler", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
-  console.log('TaskBroker')
-  const TaskBroker = await deploy('TaskBroker', {
+  });
+  console.log("TaskBroker");
+  const TaskBroker = await deploy("TaskBroker", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  }) 
-  console.log('SocialGraph')
-  const SocialGraph = await deploy('SocialGraph', {
+  });
+  console.log("SocialGraph");
+  const SocialGraph = await deploy("SocialGraph", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
-  console.log('Governance')
-  const Governance = await deploy('Governance', {
+  });
+  console.log("Governance");
+  const Governance = await deploy("Governance", {
     from: deployer,
     // args: [dataMarket.address, "DM-C-0"],
     log: true,
-  })
+  });
+
   /*
   // Verify your contracts with Etherscan
   // You don't want to verify on localhost
@@ -66,5 +84,5 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
       contractArguments: [],
     });
   } */
-}
-module.exports.tags = ['SwarmMail', 'DataHub']
+};
+module.exports.tags = ["SwarmMail", "DataHub"];
